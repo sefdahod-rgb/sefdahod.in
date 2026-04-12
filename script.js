39d5072d10c77de4794b902b5b2404c4602c28d7
Shubhi Education Foundation <sefdahod@gmail.com>
2026-04-12 18:38:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let images = [ "img1.jpg", 
-              "img2.jpg", 
-              "img3.jpg" ]; 
-let i = 0; setInterval(() => 
-  { document.getElementById("slide").src = images[i]; i = (i + 1) % images.length; }, 3000);
+const images = [
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg"
+  "img4.jpg"
+"img5.jpg"
+"poster.png"
+"poster2.png"
+];
+
+let i = 0;
+
+function startSlider(){
+
+  const imgElement = document.getElementById("slide");
+
+  if(!imgElement){
+    console.warn("Slider image element not found: #slide");
+    return;
+  }
+
+  setInterval(() => {
+    i = (i + 1) % images.length;
+    imgElement.src = images[i];
+  }, 3000);
+}
+
+// start slider when page loads
+window.onload = startSlider;
