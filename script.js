256a83653f9668bce4e372676eea1c9ff837ace1
Shubhi Education Foundation <sefdahod@gmail.com>
2026-04-12 16:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,3 @@ setInterval(() => {
   document.getElementById("slide").src = images[i];
   i = (i + 1) % images.length;
 }, 3000);
-function login(e){
-  e.preventDefault();
-
-  const u = document.getElementById("user").value;
-  const p = document.getElementById("pass").value;
-
-  if(u==="admin" && p==="admin123"){
-    localStorage.setItem("isAdmin","true");
-    alert("Login success");
-  }
-}
